1b4a0abb39009b2d1ca8cb746ccde6f272f9da0c
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-02-24 16:44:44 +0700

    create home page

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+      colors: {
+        one800: "#1b576f",
+        two500: "#12a2c4",
+        three400: "#2ec2e2",
+        four200: "#aaecf7",
+        five100: "#d2f6fb",
+      },
+    },
+  },
+  plugins: [],
+};
